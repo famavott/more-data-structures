8668ea47cc3d49a340ec9f3a197efd3808cdf270
Matt Favoino <mattfavoino@gmail.com>
2017-12-16 14:12:10 -0800

    Added if name == main block

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -18,3 +18,19 @@ def quick_sort(input_list):
         return quick_sort(less) + [pivot] + quick_sort(greater)
     else:
         raise TypeError('Only lists can be passed into function.')
+
+if __name__ == '__main__':  # pragma no cover
+    import timeit as ti
+    sort_1 = [1, 2, 4, 9, 10, 11]
+    sort_2 = [17, 9, 7, 4, 1, 0]
+
+    time_1 = ti.timeit("quick_sort(sort_1[:])",
+                       setup="from __main__ import sort_1, quick_sort")
+    time_2 = ti.timeit("quick_sort(sort_2[:])",
+                       setup="from __main__ import sort_2, quick_sort")
+    print("""
+        Input: [1, 2, 4, 9, 10, 11]
+        Good case: {}
+
+        Input: [17, 9, 7, 4, 1, 0]
+        Bad case: {}""".format(time_1, time_2))
